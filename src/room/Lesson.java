b5c6d18453ba46b3eff1693aa89beba90048056b
GevorgyanVigen <vigengevorgyan45@gmail.com>
2024-11-20 22:07:53 +0400

    barev

---
+++ src/room/Lesson.java
@@ -0,0 +1,16 @@
+package room;
+
+//bug
+public class Lesson {
+    public static void main(String[] args) {
+
+        String s = "barev ";
+        int x = 4;
+        System.out.println(s);
+        byte b = 88;
+        x = 2;
+        System.out.println("Hello java!!");
+        System.out.println(x);
+
+    }
+}
